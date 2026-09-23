e73a0fbad5e816bb993abdfb82042ec1396fed1f
Winnie-Irene <i.winnie@alustudent.com>
2026-09-23 13:20:48 +0200

    Render Task Manager in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "./context/ThemeProvider";
 import Navbar from "./components/Navbar";
+import TaskManager from "./components/TaskManager";
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <TaskManager />
     </ThemeProvider>
   );
 }
